baf54b59095de74f53de8f423e423b523289b9a7
sethrsinine <74641269+sethrsinine@users.noreply.github.com>
2025-04-18 19:19:54 +0800

    Update hellobot.py

--- hellobot.py
+++ hellobot.py
@@ -22,7 +22,7 @@ def chat_stream():
     # 发送流式请求给 OpenAI 服务器
     response = client.chat.completions.create(
         # 公司内部，一般接入的是自己的模型（垂直模型、行业模型）
-        model='gpt-3.5-turbo-16k',  # 接入的大模型  gpt-4o mini ,接入 最新的产品 能力
+        model='gpt-3.5-turbo',  # 接入的大模型  gpt-4o mini ,接入 最新的产品 能力
         messages=st.session_state.messages_history,
         temperature=0,
         stream=True  # 设置 stream=True
